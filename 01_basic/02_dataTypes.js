87f8ccb4a579a02fe51aefaa033578d2c522f01d
proprogrammer009 <149935435+proprogrammer009@users.noreply.github.com>
2023-11-07 08:23:25 +0000

    yes

--- 01_basic/02_dataTypes.js
+++ 01_basic/02_dataTypes.js
@@ -1,3 +1,24 @@
 "use strict";// treat all JS code as newer version
 
-alert("hello");
+alert("hello"); // we are using nodejs, not browser
+
+console.log(3 + 3);
+
+console.log("Hasnain");
+
+let name = "Hasnain";
+let age = 19;
+let inLogedIn = true;
+let state;
+let nam1 = 
+
+
+//number => 2 to power 53 
+//null => standalone value 
+//symbol => unique
+//string => ""
+//boolean => true/false
+//binInt, 
+//undefined 
+
+console.log(name);
