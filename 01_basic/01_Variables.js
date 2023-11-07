c623bd1e3c42a50570d7b7ece4694fb54f60b3aa
proprogrammer009 <149935435+proprogrammer009@users.noreply.github.com>
2023-11-07 05:02:08 +0000

    on this way

---
+++ 01_basic/01_Variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "hassnianrana05@gmail.com"
+var accountPassword = "123ih345"
+accountCity = "Jaranwala"
+
+// accountId = 2
+
+accountEmail = "ha@ahin.com"
+accountPassword = "32fdsfoin"
+accountCity = "Lahore"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
